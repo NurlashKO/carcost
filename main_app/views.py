ab3b6a94b7101ba06adfbc2efbf9798c95b211b7
NurlashKO <zh.nurlan96@gmail.com>
2016-07-08 21:20:27 +0600

    script added

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .cost import car_cost
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        mark = request.POST['mark'].lower()
+        model = request.POST['model'].lower()
+        is_year = request.POST['is_year'].lower()
+        return render(request, 'main_app/index.html', {'cost' : car_cost(mark, model, is_year)})
+    return render(request, 'main_app/index.html', {'cost': 0})
---
+++ scripts/cost.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def get_cost(span):
+    cost = 0
+    for v in span:
+        for ch in v:
+            if (ch.isdigit()):
+                cost = cost * 10 + int(ch)
+    return cost
+
+def cost(car_mark, car_model, car_is_year):
+
+    page = 1
+    prices = []
+    cnt = 0
+    total_sum = 0
+
+    while True :
+        domain = "http://kolesa.kz/cars/"
+        request_link = domain+car_mark+"/"+car_model+"/"+"?year[from]="+str(int(car_is_year) - 1)+"&year[to]="+str(int(car_is_year) + 1)+"&page="+str(page)
+        print(request_link)
+        page += 1
+        if (page > 10):
+            break
+
+        r = requests.get(request_link)
+        soup = bs(r.content, 'html.parser')
+        spans = soup.findAll('span', {'class' : 'price'})
+        if (len(spans) == 0):
+            break
+        prices.append(spans)
+        for span in spans:
+            print(get_cost(span))
+            total_sum += get_cost(span)
+            cnt += 1
+
+    if (cnt == 0):
+        return 0
+    return total_sum//cnt
+
+if (__name__ == "__main__"):
+    cost("toyota", "camry", "2000")
